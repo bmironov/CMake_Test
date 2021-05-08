a1c174233b7a7902815170ccd9628810a214ff4d
Boris Mironov <boris_mironov@outlook.com>
2021-05-08 17:12:31 -0400

    Add project files.

---
+++ CMake_VS_Testing2.cpp
@@ -0,0 +1,12 @@
+﻿// CMake_VS_Testing2.cpp : Defines the entry point for the application.
+//
+
+#include "CMake_VS_Testing2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
