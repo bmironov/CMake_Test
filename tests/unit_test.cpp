457c4cc487dd55974f561aa472e98c0a119b6191
Boris Mironov <boris_mironov@outlook.com>
2021-05-08 20:44:04 -0400

    Adding GoogleTest

---
+++ tests/unit_test.cpp
@@ -0,0 +1,17 @@
+
+#include "gtest/gtest.h"
+
+TEST(HelloTests, TestTrue) {
+    int n = 2;
+    ASSERT_TRUE(n == 2);
+}
+
+TEST(HelloTests, TestFalse) {
+    int n = 2;
+    ASSERT_FALSE(n == 5);
+}
+
+TEST(HelloTests, TestEq) {
+    int n = 2;
+    ASSERT_EQ(n, 2);
+}
